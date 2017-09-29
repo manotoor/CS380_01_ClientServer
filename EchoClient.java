2dc452f1b2e44a9218206bed989f019a6a884ea6
manotoor <manotoor@gmail.com>
2017-09-28 20:12:06 -0700

    Implemented multiple simultaneous clients.

--- EchoClient.java
+++ EchoClient.java
@@ -1,14 +1,9 @@
 
-import java.io.InputStream;
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+
 
 public final class EchoClient {
 
--- EchoServer.java
+++ EchoServer.java
@@ -4,37 +4,50 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 
-public final class EchoServer {
-
-    public static void main(String[] args) throws Exception {
+public class EchoServer implements Runnable{
+	
+	Socket socket;
+	EchoServer(Socket socket){
+		this.socket = socket;
+	}
+    public static void main(String[] args) throws Exception  {
+    	
         try (ServerSocket serverSocket = new ServerSocket(22222)) {
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                	String address = socket.getInetAddress().getHostAddress();
-            		System.out.printf("Client connected: %s%n", address);
-            		
-            		//Talk back to Client
-            		OutputStream os = socket.getOutputStream();
-            		PrintStream out = new PrintStream(os, true, "UTF-8");
-            		out.printf("Hi %s, thanks for connecting!%n", address);
-            		out.println("Type 'Exit' to disconnect from server");
-            		
-            		//@in InputStream for getting input from client
-            		In in = new In(socket);
-            		
-            		String s;
-            		while((s = in.readLine()) != null) {
-            			out.println(s);
-                  	}
-
-            		// close IO streams and socket
-            		System.out.printf("Closing connection: %s%n", address);
-            		out.close();
-            		in.close();
-            		socket.close();
-                }
+            	Socket socket = serverSocket.accept();
+            	new Thread(new EchoServer(socket)).start();
             }
         }
     }
+	@Override
+	public void run() {
+		try {
+		String address = socket.getInetAddress().getHostAddress();
+		System.out.printf("Client connected: %s%n", address);
+		
+		//Talk back to Client
+		OutputStream os = socket.getOutputStream();
+		PrintStream out = new PrintStream(os, true, "UTF-8");
+		out.printf("Hi %s, thanks for connecting!%n", address);
+		out.println("Type 'Exit' to disconnect from server");
+		
+		//@in InputStream for getting input from client
+		In in = new In(socket);
+		
+		String s;
+		while((s = in.readLine()) != null) {
+			out.println(s);
+      	}
+
+		// close IO streams and socket
+		System.out.printf("Closing connection: %s%n", address);
+		out.close();
+		in.close();
+		socket.close();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		
+	}
 }
 
